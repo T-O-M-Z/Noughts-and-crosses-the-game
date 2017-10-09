59688cf0d713ec4cda18dce6aae4ce1b7080acad
TomZ <thomas.zazzeroni@bedmod.co.uk>
2017-10-09 10:34:04 +0100

    9-10-17

--- src/OX/Main.java
+++ src/OX/Main.java
@@ -1,11 +1,38 @@
 package OX;
+import java.util.Scanner;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+public class Main
+{
+    public static void delay(int millis)
+    {
+        try
+        {
+            Thread.sleep(millis);
+        }
+        catch (InterruptedException exp)
+        {
+        }
     }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Welcome to 'Noughts&Crosses - The Game'");
+        delay(500);
+        Scanner theirdecision = new Scanner(System.in);
+        System.out.print("To begin, start by selecting your shape. Choose either X or O: ");
+        theirdecision.nextLine();
+        String thechoice = theirdecision.nextLine();
+        System.out.print("You entered " + thechoice);
+        delay(100);
+        System.out.println("Let's begin ");
+        System.out.println(".");
+        delay(500);
+        System.out.println(".");
+        delay(500);
+        System.out.println(".");
+        delay(500);
+    }
+
+
+
 }
